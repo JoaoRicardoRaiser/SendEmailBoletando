f55f1fa7357558a18afce15aad70cc11a7cfaac6
João Ricardo Raiser <joaorraiser@gmail.com>
2022-03-01 17:12:10 -0300

    ajuste sem web

--- app.py
+++ app.py
@@ -1,27 +1,21 @@
 import datetime
+import os
 import time
-
-import requests
-from flask import Flask
 
 from src.notificacao_boletando_service import NotificationBoletandoService
 
-app = Flask(__name__)
 
-
-@app.route("/")
-def index():
+def run():
     notificationService = NotificationBoletandoService()
+    should_check_new_promotions = bool(os.getenv("SHOULD_CHECK_NEW_PROMOTIONS"))
 
-    for i in range(5):
+    while should_check_new_promotions:
         print(f"Iniciando busca as: {datetime.datetime.now()}")
         notificationService.check_new_promotions()
         print(f"Finalizando busca as: {datetime.datetime.now()}")
-        time.sleep(1)
 
-    requests.get("https://notificacao-boletando.herokuapp.com/")
-    return "Realizada com sucesso."
+        time.sleep(5)
 
 
 if __name__ == '__main__':
-    app.run()
+    run()
